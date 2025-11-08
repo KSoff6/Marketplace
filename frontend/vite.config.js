24c084de3e4293605426114a77125754fcccc8df
WerLym <sarmat-kozyrev@mail.ru>
2025-11-08 15:32:41 +0300

    started making the project structure

---
+++ frontend/vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(path.__dirname, "src"),
+      "@components": path.resolve(path.__dirname, "src/components"),
+      "@assets": path.resolve(path.__dirname, "src/assets"),
+    },
+  },
+});
